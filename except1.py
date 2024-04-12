9eed5c33ec3e74c76d73b293a4790455d970a60c
Haria.Informatica <haria.informatica@gmail.com>
2024-04-12 12:26:24 +0100

    añadido excepciones

--- except1.py
+++ except1.py
@@ -3,16 +3,29 @@ import random
 def intDiv(a:int, b:int)->int:
     try:
         return a // b
-    except:
-        print("Realizamos tareas de control de cierre")
+    except TypeError:
+        print("TypeError Realizamos tareas de control de cierre")
+    except ZeroDivisionError:
+        print("ZeroDivisionError Realizamos tareas de control de cierre")
+    except Exception:
+        print("Exception Realizamos tareas de control de cierre")
 
 
 #Si es el flujo principal
 if __name__ == "__main__":
-    '''
-    for i in range(30):
-        a = random.randint(0,9)
-        b = random.randint(0,9)
-        print("a:", a, "b:", b, "a // b:", intDiv(a, b))
-    '''
-    intDiv(2, "3")
+
+    def main()->None:
+        for i in range(30):
+            a = random.randint(0,9)
+            b = random.randint(0,9)
+            print("a:", a, "b:", b, "a // b:", intDiv(a, b))
+
+        intDiv(2, "3")
+
+        try:
+            intDiv()
+        except:
+            print("No lo consiguió")
+
+    
+    main()
