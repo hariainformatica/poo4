17e3c4c63a20cb01425eb065e3e4810855d1ccd6
Haria.Informatica <haria.informatica@gmail.com>
2024-04-15 13:04:57 +0100

    añadido control excepciones

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,7 +1,16 @@
 import pytest
 
+def divide(a:int, b:int):
+    if (b == 0):
+        raise ValueError("Cannot divide by Zero")
+
 def test_exception():
     a = 0
     b = 10
     with pytest.raises(ZeroDivisionError):
         b / a
+
+def test_divide():
+    with pytest.raises(ValueError) as exception_info:
+        divide(10,0)
+    assert str(exception_info.value).lower == "Cannot divide by Zero".lower
