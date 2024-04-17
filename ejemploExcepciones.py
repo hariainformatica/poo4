5a0c53646bcb4d8e3be45cb3b5ff02123ab3760b
Haria.Informatica <haria.informatica@gmail.com>
2024-04-17 09:36:15 +0100

    primer test ejemploExcepciones

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -1,5 +1,10 @@
 class EjemploExcepciones:
     #ZeroDivisionError
+    def zeroDivisionError(self, *, num:int, den:int)->int:
+        if (den == 0):
+            raise ZeroDivisionError
+        
+        return num // den
 
     #ValueError
 
