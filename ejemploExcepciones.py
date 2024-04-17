5525bf2e9e796d10dc24066f346e36b1f411ff4a
Haria.Informatica <haria.informatica@gmail.com>
2024-04-17 12:45:19 +0100

    añadido tests a test_ejemploExcepciones.py

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -7,17 +7,32 @@ class EjemploExcepciones:
         return num // den
 
     #ValueError
-
+    def valueError(self):
+        raise ValueError
     #FileNotFoundError
+    def fileNotFoundError(self):
+        raise FileNotFoundError
 
     #TypeError
+    def typeError(self):
+        raise TypeError
 
     #PermissionError
+    def permissionError(self):
+        raise PermissionError
 
     #IndexError
+    def indexError(self):
+        raise IndexError
 
     #KeyboardInterrupt
+    def keyboardInterrupt(self):
+        raise KeyboardInterrupt
 
     #UnicodeDecodeError
+    def unicodeDecodeError(self):
+        raise UnicodeDecodeError
 
     #AttributeError
+    def attributeError(self):
+        raise AttributeError
