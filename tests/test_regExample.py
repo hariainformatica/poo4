126a4e31169e1746c09139c04c7a9e91e914a997
Haria.Informatica <haria.informatica@gmail.com>
2024-05-07 12:29:04 +0100

    arreglado test restExample

--- tests/test_regExample.py
+++ tests/test_regExample.py
@@ -8,5 +8,5 @@ def test_url():
     assert RegExample.validURL("google") == False
     assert RegExample.validURL("https://www.google.es") == True
     assert RegExample.validURL("http://127.0.0.1") == True
-    assert RegExample.validURL("http://dominio.com/carpeta/archivo.ext")
-    assert RegExample.validURL("https://consuma.api/carpeta/archivo.ext#atr=2&uno=5")
+    assert RegExample.validURL("http://dominio.com/carpeta/archivo.ext") == True
+    assert RegExample.validURL("https://consuma.api/carpeta/archivo.ext#atr=2&uno=5") == True
